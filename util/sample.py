f99a77d3abd6dcd44253e60f8f4867a2c98bfc1d
xDroid <9379520+owen8877@users.noreply.github.com>
2022-03-31 15:16:12 -0500

    Skeleton code form BM sampling.

---
+++ util/sample.py
@@ -0,0 +1,22 @@
+from unittest import TestCase
+
+import numpy as np
+
+
+def geometricBM(T: int, N: int, mu: np.float, sigma: np.float):
+    """
+    Generate ...
+    :param T:
+    :param N:
+    :param mu:
+    :param sigma:
+    :return:
+    """
+    pass
+
+
+class Test(TestCase):
+    def test_geometricBM(self):
+        # some tests showing that the function is valid...
+        # use matplotlib to plot sampled trajectories
+        pass
