6a9ca1a263d416a94adc44c75a43f41222484943
owen8877 <9379520+owen8877@users.noreply.github.com>
2022-04-05 00:09:50 -0500

    Create a draft version of RLOP environment.

---
+++ util/__init__.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+def standard_to_normalized_price(standard_price: np.ndarray, mu: np.sctypes, sigma: np.sctypes, time: np.ndarray):
+    return (-(mu - sigma ** 2 / 2) * time + np.log(standard_price)) / sigma
+
+
+def normalized_to_standard_price(normalized_price: np.ndarray, mu: np.sctypes, sigma: np.sctypes, time: np.ndarray):
+    return np.exp(sigma * normalized_price + (mu - sigma ** 2 / 2) * time)
+
+
+def payoff_of_option(is_call_option: bool, asset_price: np.sctypes, strike_price: np.sctypes):
+    if is_call_option:
+        return np.max([0, asset_price - strike_price])
+    else:
+        return np.max([0, strike_price - asset_price])
