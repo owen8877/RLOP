3e2acac11b4f2f1f9fc18ca12d03c9ce44fdd0b7
specfazhou <86537418+specfazhou@users.noreply.github.com>
2022-04-05 21:23:58 -0500

    implemented brownian motion, geometric brownian motion, and Black Scholes formula

---
+++ util/pricing.py
@@ -0,0 +1,36 @@
+from unittest import TestCase
+
+import numpy as np
+import scipy.stats as si
+
+def bs_euro_vanilla_call(S, K, T, r, sigma):
+    """
+    :param S: current stock price
+    :param K: strike price
+    :param T: maturity date
+    :param r: risk-free rate
+    :param sigma: volatility
+    :return: option price
+    """
+    d1 = (np.log(S / K) + (r + 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
+    d2 = (np.log(S / K) + (r - 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
+    call = (S * si.norm.cdf(d1, 0.0, 1.0) - K * np.exp(-r * T) * si.norm.cdf(d2, 0.0, 1.0))
+    return call
+
+def bs_euro_vanilla_put(S, K, T, r, sigma):
+
+    d1 = (np.log(S / K) + (r + 0.5 * sigma * sigma) * T) / (sigma * np.sqrt(T))
+    d2 = (np.log(S / K) + (r - 0.5 * sigma * sigma) * T) / (sigma * np.sqrt(T))
+    put = (K * np.exp(-r * T) * si.norm.cdf(-d2, 0.0, 1.0) - S * si.norm.cdf(-d1, 0.0, 1.0))
+    return put
+
+
+class Test(TestCase):
+    def test_call(self):
+        a = bs_euro_vanilla_call(100, 120, 1, 0.05, 0.25)
+        print(a)
+
+    def test_put(self):
+        b = bs_euro_vanilla_put(120, 120, 1, 0.05, 0.25)
+        print(b)
+
--- util/sample.py
+++ util/sample.py
@@ -1,22 +1,89 @@
 from unittest import TestCase
 
 import numpy as np
+from scipy.stats import norm
+import matplotlib.pyplot as plt
 
 
-def geometricBM(T: int, N: int, mu: np.float, sigma: np.float):
+def brownian(x0, n, dt, out=None):
     """
-    Generate ...
-    :param T:
-    :param N:
-    :param mu:
-    :param sigma:
-    :return:
+    :param x0: float
+     the initial value of stock price
+    :param n: int
+     the number of steps to take
+    :param dt: float
+     the time step
+    :param out: numpy array or None
+     If `out` is not None, it specifies the array in which to put the
+     result.  If `out` is None, a new numpy array is created and returned.
+    :reutrns: a numpy array of length n+1 with the first element x0
     """
-    pass
+    r = norm.rvs(size=n, scale=np.sqrt(dt))
+    if out is None:
+        out = np.empty(n + 1)
+
+    out[0] = x0
+
+    for i in range(1, n + 1):
+        out[i] = out[i - 1] + r[i - 1]
+
+    return out
+
+
+def geometricBM(x0: np.float, T: int, N: int, mu: np.float, sigma: np.float):
+    """
+    generate one trajectory of geometric brownian motion with N steps with time step T
+    :param T: total steps
+    :param N: number of trajectories
+    :param mu: drift term
+    :param sigma: volatility term
+    :return: a numpy array of length N+1
+    x_t = x_0 * exp( (mu - sigma^2 / 2) t + sigma * W_t)
+    """
+    GBM_trajectories = np.zeros([N, T + 1])
+    for i in range(N):
+        samples = brownian(0, T, 1)
+        GBM_trajectories[i, :] = x0 * np.exp((mu - sigma * sigma / 2) * np.arange(T + 1) + sigma * samples)
+
+    return GBM_trajectories
 
 
 class Test(TestCase):
-    def test_geometricBM(self):
+    def test_BM(self):
         # some tests showing that the function is valid...
         # use matplotlib to plot sampled trajectories
+        k = 500
+        dt = 0.05
+        n = 20
+        x = [0]
+        bm_final = []
+        var = 0
+
+        for j in range(1, n + 1):
+            x.append(j * dt)
+
+        for i in range(k):
+            bm = brownian(0, n, dt)
+            bm_final.append(bm[n])
+
+            bm_list = bm.tolist()
+            plt.plot(x, bm_list)
+
+        print(np.average(bm_final))
+        print(np.var(bm_final))
+        plt.show()
+
+    def test_geometricBM(self):
+        a = geometricBM(10, 20, 50, 0.2, 0.1)
+        x = np.arange(21)
+
+        for i in range(50):
+            y = a[i, :]
+            plt.plot(x,y)
+
+        plt.show()
+
+
+
+
         pass
